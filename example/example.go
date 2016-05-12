239016fe1b6dfa308febd23b761e6494ebd664ab
nikkolasg <nikkolasg@gmail.com>
2016-05-12 11:23:03 +0200

    printout

--- example/example.go
+++ example/example.go
@@ -8,23 +8,25 @@ func TestHostCheckNet() {
 
 	valid_ip := "128.30.52.45:80" // w3.org
 	if err := checker.CheckTCP(valid_ip); err != nil {
-		fmt.Println("Should be valid")
+		fmt.Println("Should be valid", err)
 	}
 
 	invalid_ip := "128.30.52.45:678"
 	if err := checker.CheckTCP(invalid_ip); err == nil {
-		fmt.Println("Should be invalid")
+		fmt.Println("Should be invalid", err)
 	}
 }
 
 func TestWhatsMyIP() {
 	checker := ipreach.WhatsMyIp{}
 	if err := checker.CheckTCP("127.0.0.1:3100"); err == nil {
-		fmt.Println("Should not work if port not open")
+		fmt.Println("[-] Should not work if port not open")
 	}
+	fmt.Println("[+] 3100 port not open correctly detected!")
 	if err := checker.CheckTCP("127.0.0.1:3000"); err != nil {
 		fmt.Println("Should not work if port not open", err)
 	}
+	fmt.Println("[+] 3000 port OPEN correctly detected!")
 }
 
 func main() {
